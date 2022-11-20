d8095985e0edb9fa14dac3975bbd5ae8ce7adcae
Naveen Doddi <109824438+NaveenDoddi@users.noreply.github.com>
2022-11-20 12:13:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function run(){
     }
     
 }
+function run2(){
+    var input = Math.floor(Math.random() * 16) + 1
+    for(let i=1; i<=16; i++){
+        var tdvalue = document.getElementById("dump2"+i).innerText
+        var tdid = document.getElementById("dump2"+i)
+        if(input==tdvalue){
+            (tdid.style.backgroundColor="red")
+        }else{
+            tdid.style.backgroundColor="white"
+        }
+      document.getElementById("center2").innerText=input
+    }
+
+}
+
+let fun=setInterval(run2, 200)
+function start2(){
+    window.location.reload()
+}
+
+function stop2(){
+    clearInterval(fun)
+}
